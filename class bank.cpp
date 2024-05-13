3053cf6a1c46eee49a873767f8037c92dc3fd123
Siddhant Bagga <siddhantbagga2022@vitbhopal.ac.in>
2024-05-13 23:26:39 +0530

    Add files via upload

---
+++ class bank.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class bank
+{
+    class var: acc_n0, acc_name, acc_bal
+    ACC_OPEN(){acc_no, acc_name}
+    //b1.acc_no+23
+    dept(){}
+    withdraw(){}
+    chk_bal(){}
+
+};
+main()
+{
+    bank b1,b2...b100
+    b1.ACC_OPEN();
+    b1.dept(a,b);
+    b1.withdraw();
+    b1.chk_bal();
+}
